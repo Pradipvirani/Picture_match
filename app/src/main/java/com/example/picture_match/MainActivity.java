dd0fae70dd2adc497c2d750de36fb02c29fd205d
HP <pureb88@gmail.com>
2023-04-21 17:40:21 +0530

    1

---
+++ app/src/main/java/com/example/picture_match/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.picture_match;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button1,button2,button3,button4,button5,button6;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1=findViewById(R.id.no_time);
+        button2=findViewById(R.id.normal);
+        button3=findViewById(R.id.hard);
+        button4=findViewById(R.id.remove_ads);
+        button5=findViewById(R.id.share);
+        button6=findViewById(R.id.more_games);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==button1.getId())
+        {
+
+        }
+        if (view.getId()==button2.getId())
+        {
+
+        }
+        if (view.getId()==button3.getId())
+        {
+
+        }
+        if (view.getId()==button4.getId())
+        {
+
+        }
+        if (view.getId()==button5.getId())
+        {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,"my application");
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
+
+        }
+        if (view.getId()==button6.getId())
+        {
+
+        }
+
+    }
+}
