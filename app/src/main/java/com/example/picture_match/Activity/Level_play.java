2dd03cc14beb183ba50003b3f35c37e8b4818489
HP <pureb88@gmail.com>
2023-05-02 16:40:04 +0530

    120

--- app/src/main/java/com/example/picture_match/Activity/Level_play.java
+++ app/src/main/java/com/example/picture_match/Activity/Level_play.java
@@ -2,17 +2,48 @@ package com.example.picture_match.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.example.picture_match.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Level_play extends AppCompatActivity implements View.OnClickListener {
+    int level;
+    String status;
+    ArrayList<String>imagearr=new ArrayList<>();
+    List<String>arrayliost=new ArrayList<>();
+    ProgressBar progressBar;
+    GridView gridView;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    int numimage,colum;
+    TextView textView;
+    Toolbar toolbar;
+    Button back,go;
+    TextView textView1,textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_play);
+        level=getIntent().getIntExtra("level",0);
+        status=getIntent().getStringExtra("status");
+        progressBar=findViewById(R.id.progress);
+        gridView=findViewById(R.id.grid_view_play);
+        preferences=getSharedPreferences("pre",MODE_PRIVATE);
+        editor=preferences.edit();
+
     }
 
     @Override
